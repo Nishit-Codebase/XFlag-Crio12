3d92e4a3a033edcb89f71b90dcf126e69c8aa6bf
Nishit Rana <nishitrana@Nishits-MacBook-Air.local>
2025-02-28 15:30:58 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,49 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const[User,Setuser] = useState("");
-  const[Pass,Setpass] = useState("");
-  const [flag,Setflag] = useState(null);
+  const [User, Setuser] = useState("");
+  const [Pass, Setpass] = useState("");
+  const [flag, Setflag] = useState(null);
 
-  let Authenticate = (e) =>{
+  let Authenticate = (e) => {
     e.preventDefault();
-     if(User === "user" && Pass === "password")
-     {
+    if (User === "user" && Pass === "password") {
       Setflag(true);
-     }else{
+    } else {
       Setflag(false);
-     }
-  }
+    }
+  };
   return (
     <>
-    <h1>Login Page</h1>
+      <h1>Login Page</h1>
 
-    {flag === false && <p>Invalid username or password</p>}
-    {flag === true && <h2>Welcome, {User}!</h2>}
-    <form onSubmit={Authenticate}>
-    <label>
-      Username
-      <input required onChange={(e) => Setuser(e.target.value)} placeholder="Enter username" />
-    </label>
-    <br />
-    <label>
-      Password
-      <input required type="password" onChange={(e) => Setpass(e.target.value)} placeholder="Enter password" />
-    </label>
-    <br />
-    <button type='submit'>Submit</button>
-    </form>
-   
-  </>
-  )
+      {flag === false && <p>Invalid username or password</p>}
+      {flag === true && <h2>Welcome, {User}!</h2>}
+      <form onSubmit={Authenticate}>
+        <label htmlFor="name">Username</label>
+        <input
+          required
+          id="name"
+          value={User}
+          onChange={(e) => Setuser(e.target.value)}
+          placeholder="Enter username"
+        />
+
+        <br />
+        <label htmlFor="pass">Password</label>
+        <input
+          id="pass"
+          required
+          value={Pass}
+          type="password"
+          onChange={(e) => Setpass(e.target.value)}
+          placeholder="Enter password"
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
-export default App
+export default App;
