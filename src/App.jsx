54005ea82fff48d2fec0b2822d959fb52f3df9cb
Nishit Rana <nishitrana@Nishits-MacBook-Air.local>
2025-02-28 14:52:18 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,40 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
   const[User,Setuser] = useState("");
   const[Pass,Setpass] = useState("");
   const [flag,Setflag] = useState(null);
 
-  let Authenticate = () =>{
-
+  let Authenticate = (e) =>{
+    e.preventDefault();
      if(User === "user" && Pass === "password")
      {
       Setflag(true);
      }else{
       Setflag(false);
      }
-
   }
-
   return (
     <>
     <h1>Login Page</h1>
 
-    {/* Show error message above the form if login is incorrect */}
     {flag === false && <p>Invalid username or password</p>}
     {flag === true && <h2>Welcome, {User}!</h2>}
+    <form onSubmit={Authenticate}>
     <label>
-      Username:
+      Username
       <input required onChange={(e) => Setuser(e.target.value)} placeholder="Enter username" />
     </label>
     <br />
     <label>
-      Password:
+      Password
       <input required type="password" onChange={(e) => Setpass(e.target.value)} placeholder="Enter password" />
     </label>
     <br />
-    <button onClick={Authenticate}>Submit</button>
-
-    {/* Show "Welcome, user!" below the form if login is successful */}
+    <button type='submit'>Submit</button>
+    </form>
    
   </>
   )
 }
-
 export default App
